5cb71004c624674d4a9c5743faaa31f1e9703641
Bby Lion <codelab042@gmail.com>
2026-06-10 04:04:32 +0100

    Your commit message goes here

--- components/landing-page/Sidebar.tsx
+++ components/landing-page/Sidebar.tsx
@@ -51,9 +51,9 @@ export default function Sidebar() {
               <AnimatePresence mode="wait">
                 <motion.div
                   key={currentImage}
-                  initial={{ opacity: 0, x: 30 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  exit={{ opacity: 0, x: -30 }}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
                   transition={{ duration: 0.8, ease: "easeInOut" }}
                   className="absolute inset-0"
                 >
